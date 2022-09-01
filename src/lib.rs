e8b663680879db7877f9f389cc735e65517ceb29
Pablo <lozano.p15@gmail.com>
2022-09-01 15:02:36 +0200

    stopped at 3.7.3.3 (not included)

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 // }
 
 // // We need to mark `run` as public.
-// // Befora, it was no longer a binary entrypoint, therefore we can mark it as async
+// // Before, it was no longer a binary entrypoint, therefore we can mark it as async
 // // without having to use any proc-macro incantation.
 // // We return ``Server` on the happy path and we dropped the `async` keyword.
 // // Notice the different signature!
@@ -35,10 +35,25 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+
     Ok(server)
 
     // // HttpServer: In charge of all transport level concerns
